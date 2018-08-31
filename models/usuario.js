ea67e75103943bf6f09d319bd0f9e6bd386aeec8
Martin Vega <martin_vega@yahoo.com>
2018-08-30 19:37:39 -0500

    Servicios iniciales

---
+++ models/usuario.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var rolesValidos = {
+    values: ['ADMIN_ROLE','USER_ROLE','SPONSOR_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+
+};
+
+var usuarioSchema = new Schema ({
+
+    nombre: { type: String, required: [true, 'El nombre es necesario']},
+    email:  { type: String, unique: true, required: [true,'El correo es necesario']},
+    password: { type: String, required: false},
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+    usuario:  { type: String, required: [true,'Usuario es requerido']},
+    sponsor:  { type: String, required: [true,'El patrocinador es necesario']}
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: ' {PATH} debe ser unico'});
+
+module.exports = mongoose.model('Usuario',usuarioSchema);
