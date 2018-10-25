16f5bc087bb1c0a459ac8e4b60fcba696105ee72
Martin Vega <martin_vega@yahoo.com>
2018-10-25 18:42:00 -0500

    chage login

--- routes/login.js
+++ routes/login.js
@@ -27,6 +27,54 @@ app.get('/renuevatoken', mdAutenticacion.verificaToken, ( req, res )=>{
 app.post('/', (req, res)=>{
     var body = req.body;
 
+    Usuario.findOne({ usuario: body.usuario}, (err,usuarioDB)=> {
+        if (err) {
+            return res.status(500).json({    
+                ok: false,    
+                mensaje: 'Error al buscar usuarios',    
+                errors: err    
+            });    
+            
+        }   
+        if (!usuarioDB){
+            return res.status(400).json({    
+                ok: false,    
+                mensaje: 'Credenciales incorrectas - usuario ' ,  
+                errors: err    
+            });  
+        }
+
+        if( !bcrypt.compareSync( body.password, usuarioDB.password )){
+            return res.status(400).json({    
+                ok: false,    
+                mensaje: 'Credenciales incorrectas - password ' ,  
+                errors: err    
+            });  
+
+        }
+
+        //Crear un token
+        var config = require('../config/config');
+        var token = jwt.sign({usuario:usuarioDB}, config.SEED,{ expiresIn: 14400 }); //4horas
+        usuarioDB.password = ':)';
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+            token: token,
+            id: usuarioDB._id
+        });
+
+    });
+ 
+
+});
+
+// ==============================
+// Login de usuario  --anterior con email
+// ==============================
+/*app.post('/', (req, res)=>{
+    var body = req.body;
+
     Usuario.findOne({ email: body.email}, (err,usuarioDB)=> {
         if (err) {
             return res.status(500).json({    
@@ -68,5 +116,6 @@ app.post('/', (req, res)=>{
  
 
 });
+*/
 
 module.exports = app;
